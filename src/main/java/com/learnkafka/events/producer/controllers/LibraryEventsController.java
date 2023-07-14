4c173745af3d4ed31d5dd89bb527eb627bcb0c0d
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2023-07-13 21:51:37 -0500

    Producer integration to controller

--- src/main/java/com/learnkafka/events/producer/controllers/LibraryEventsController.java
+++ src/main/java/com/learnkafka/events/producer/controllers/LibraryEventsController.java
@@ -1,7 +1,9 @@
 package com.learnkafka.events.producer.controllers;
 
+import com.learnkafka.events.producer.LibraryEventsProducer;
 import com.learnkafka.events.producer.dtos.LibraryEventDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class LibraryEventsController {
 
+    private final LibraryEventsProducer producer;
+
+    @Autowired
+    public LibraryEventsController(LibraryEventsProducer producer) {
+        this.producer = producer;
+    }
+
     @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<LibraryEventDTO> postLibraryEvent(@RequestBody LibraryEventDTO requestBody) {
         log.info("libraryEvent: {}", requestBody);
-        // TODO: Invoke Kafka Producer
+        producer.sendLibraryEvent(requestBody);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(requestBody);
     }
